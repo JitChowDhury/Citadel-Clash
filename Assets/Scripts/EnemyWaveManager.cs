5a3f76ed36e94b1c88d67f299e7077d256f0cef8
JitChowDhury <jitchowdhury8717@gmail.com>
2025-07-17 22:48:33 +0530

    Final Base Logic

--- Assets/Scripts/EnemyWaveManager.cs
+++ Assets/Scripts/EnemyWaveManager.cs
@@ -2,43 +2,66 @@ using UnityEngine;
 
 public class EnemyWaveManager : MonoBehaviour
 {
+
+    private enum State
+    {
+        WaitingToSpawnNextWave,
+        SpawningWave,
+    }
+
+    [SerializeField] Transform spawnPositionTranform;
+    private State state;
     private float nextWaveSpawnTimer;
     private float nextEnemySpawnTimer;
     private int remainingEnemySpawnAmount;
     private Vector3 spawnPos;
     void Start()
     {
+        state = State.WaitingToSpawnNextWave;
         nextWaveSpawnTimer = 3f;
     }
 
     void Update()
     {
-        nextWaveSpawnTimer -= Time.deltaTime;
-        if (nextWaveSpawnTimer < 0f)
+        switch (state)
         {
+            case State.WaitingToSpawnNextWave:
 
-            SpawnWave();
+                nextWaveSpawnTimer -= Time.deltaTime;
+                if (nextWaveSpawnTimer < 0f)
+                {
+
+                    SpawnWave();
+                }
+                break;
+            case State.SpawningWave:
+                if (remainingEnemySpawnAmount > 0)
+                {
+                    nextEnemySpawnTimer -= Time.deltaTime;
+                    if (nextEnemySpawnTimer < 0f)
+                    {
+                        nextEnemySpawnTimer = Random.Range(0f, .2f);
+                        Enemy.Create(spawnPos + UtilsClass.GetRandomDir() * Random.Range(0f, 10f));
+                        remainingEnemySpawnAmount--;
+
+                        if (remainingEnemySpawnAmount <= 0f)
+                            state = State.WaitingToSpawnNextWave;
+
+                    }
+                }
+                break;
         }
 
-        if (remainingEnemySpawnAmount > 0)
-        {
-            nextEnemySpawnTimer -= Time.deltaTime;
-            if (nextEnemySpawnTimer < 0f)
-            {
-                nextEnemySpawnTimer = Random.Range(0f, .2f);
-                Enemy.Create(spawnPos + UtilsClass.GetRandomDir() * Random.Range(0f, 10f));
-                remainingEnemySpawnAmount--;
 
-            }
-        }
     }
 
     public void SpawnWave()
     {
-        Vector3 spawnPos = new Vector3(40, 0);
+        spawnPos = spawnPositionTranform.position;
 
         nextWaveSpawnTimer = 10f;
         remainingEnemySpawnAmount = 10;
+        state = State.SpawningWave;
 
     }
 
