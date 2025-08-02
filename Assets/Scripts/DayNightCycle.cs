5ed4cf7c026b9649f6791c8307d3fae01f290cd5
JitChowDhury <jitchowdhury8717@gmail.com>
2025-08-02 22:38:25 +0530

    Day Night Cycle

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [SerializeField] private Gradient gradient;
+    [SerializeField] private float secondsPerDay = 10f;
+
+
+    private Light2D light2D;
+    private float dayTime;
+    private float dayTimeSpeed;
+
+
+    void Awake()
+    {
+        light2D = GetComponent<Light2D>();
+        dayTimeSpeed = 1 / secondsPerDay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dayTime += Time.deltaTime * dayTimeSpeed;
+        light2D.color = gradient.Evaluate(dayTime % 1f);
+    }
+}
